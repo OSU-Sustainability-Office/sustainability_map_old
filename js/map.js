dfb377fcef57b734d10bb7fceb61680c66597015
jackrwoods <jackrwoods@gmail.com>
2018-05-14 10:26:32 -0700

    Add leaflet API and mapbox tiles.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,13 @@
-// initialize the map on the "map" div with a given center and zoom
+// Initialize the map on the "map" div with a given center and zoom
 var map = L.map('map', {
-    center: [44.5633194, -123.277909],
-    zoom: 13
+    center: [44.565, -123.2785],
+    zoom: 17
 });
+
+// Add the map tiles
+L.tileLayer('https://api.mapbox.com/styles/v1/jack-woods/cjh6io71w09s42sqr6rizxwkm/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFjay13b29kcyIsImEiOiJjamg2aWpjMnYwMjF0Mnd0ZmFkaWs0YzN0In0.qyiDXCvvSj3O4XvPsSiBkA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiamFjay13b29kcyIsImEiOiJjamg2aWpjMnYwMjF0Mnd0ZmFkaWs0YzN0In0.qyiDXCvvSj3O4XvPsSiBkA'
+}).addTo(map);
