b2f1ee8bac0e3903fcaf5aacda2f72ad22e8d037
Brogan Miner <BroganMiner22@gmail.com>
2018-08-07 12:01:19 -0700

    test build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/sustainability_map/',
+    assetsPublicPath: 'sustainability_map',
 
     /**
      * Source Maps
