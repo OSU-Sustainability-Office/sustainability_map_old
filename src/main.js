4f2ab8f45223f43450f0f61c9f291f1e6ce24c10
jackrwoods <jackrwoods@gmail.com>
2018-08-20 15:07:48 -0700

    Fix typo. store.js should not be capitalized in main.js.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import StoreJS from './Store'
+import StoreJS from './store'
 import router from './router'
 import VueI18n from 'vue-i18n'
 import vuex from 'vuex'
